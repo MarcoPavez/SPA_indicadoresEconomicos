68910f8cf861e1e090161c2ad6b5c1b45ac8f8df
Marco Pavez <marcopz03@outlook>
2023-01-03 15:29:16 -0300

    conexion servidor, consumo de API y visualizacion de datos backend en frontend

---
+++ cliente/src/pages/HomePage.js
@@ -0,0 +1,13 @@
+import React from "react";
+import IndicadoresComponent from "../components/IndicadoresComponent";
+
+const HomePage = () => {
+    return(
+        <div>
+            <IndicadoresComponent/>
+        </div>
+    )
+}
+
+export default HomePage;
+
